8b5d70350ec279eeaa1c85792b4c7bb6a90d2bc0
sami157 <sami.eee157@gmail.com>
2025-12-06 23:29:50 +0600

    deployment ready

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import { initDB } from "./database/db"
 import dotenv from 'dotenv'
 import { vehicleRouter } from "./modules/vehicles/vehicles.router"
@@ -16,6 +16,10 @@ app.use('/api/v1/vehicles', vehicleRouter)
 app.use('/api/v1/bookings', bookingRouter)
 
 initDB()
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to this Server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
